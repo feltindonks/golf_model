d11b655dffe962b1023f74fc18535dd81ffb7b14
feltindonks <78088174+feltindonks@users.noreply.github.com>
2021-04-30 14:17:18 -0500

    Update inv.py

--- inv.py
+++ inv.py
@@ -3,25 +3,31 @@ import pandas as pd
 import csv
 import argparse
 
-file_name = 'rbc_heritage_win_european.csv'
-top = 1
+file_name = 'valspar_championship_win_european.csv'
+mod_file = str('edit_' + str(file_name))
+#top = 1
 df = pd.read_csv(file_name)
 #drops ev columns
 df = df[df.columns.drop(list(df.filter(regex='_ev')))]
+
 #while loop that converts odds to probabilties
 cur_col = 2
 num_col = (len(df.columns) - 1)
 while cur_col <= num_col:
-    df[str(cur_col - 1) + '_inv'] = df.iloc[:,cur_col].pow(-1)
+    df[str(df.columns[cur_col])[:5] + '_%'] = df.iloc[:, cur_col].pow(-1)
     cur_col += 1
+
 #drop odds columns
-# df = df[df.columns.drop(list(df.filter(regex='_odds')))]
+df = df[df.columns.drop(list(df.filter(regex='_odds')))]
 #change inf to nan
 df.replace([np.inf, -np.inf], np.nan, inplace=True)
 
-df['ave_VF'] = (df['2_inv'] /  df['2_inv'].sum())
-print (file_name)
-sums  = df.sum(axis = 0, skipna = True, numeric_only = True)
-print (sums / top)
-print (df)
-#print ("inv_df:", df)
+#sums  = df.sum(axis = 0, skipna = True, numeric_only = True)
+#print (sums / top)
+print (df.head())
+print('find csv @ ' + mod_file)
+df.to_csv(mod_file, index=False) #index=False means no index column @ col[0]
+
+#below an example from cheatsheet
+#Append a column of row sums to a DataFrame
+#df['Total'] = df.sum(axis=1)
